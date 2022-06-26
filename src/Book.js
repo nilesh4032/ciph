56c36088251f4cd733db0c45fe28a5c057368ab4
haymaker00 <nileshkumar4032@gmail.com>
2022-06-26 18:15:01 +0530

    1st

---
+++ src/Book.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Book = ({img,title,author}) => {
+  //const {img,title,author}=props;
+    const clickhandler=()=>{
+        alert('bought')
+    }
+return(
+    <artcile className="book" onMouseOver={()=>{ console.log(title)}}>
+         <h1 style={{margin: "2rem"}}> {img} </h1>
+        <h1 style={{margin: "2rem"}}> {title}</h1>  
+        <h3 onClick={()=>{ console.log('alert');}}>{author}</h3>
+        <button type='button' onClick={clickhandler} style={{margin: "2rem "}}>buy </button>
+        </artcile> 
+)
+}
+
+export default Book
---
+++ src/books.js
@@ -0,0 +1,29 @@
+export const books=[             
+ {
+id:1,
+img:'Nord Stage 3 88 keys',
+title:'E.M.L.A Music',
+author:'$5,738.00 Aud'
+
+},
+{
+id:2,
+img:'Line 6 Helix Rack Guitar Processor - 1x',
+title:'ProMusic',
+author:'$1,831.57 Aud'
+
+},
+{
+id:3,
+img:'Nord Lead A1',
+title:'Gear Garage',
+author:'$1700 Aud'
+
+},
+{
+id:1,
+img:'Moog Subsequent 37',
+title:'Turnlab',
+author:'$5,738 Aud'
+}
+];
