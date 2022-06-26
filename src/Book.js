f00022a147b241513c3014461f511736ab549c96
haymaker00 <nileshkumar4032@gmail.com>
2022-06-26 19:01:35 +0530

    4th

--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-const Book = ({img,title,author}) => {
+const Book = ({pic,img,title,author}) => {
   //const {img,title,author}=props;
     const clickhandler=()=>{
         alert('done')
     }
 return(
     <artcile className="book" onMouseOver={()=>{ console.log(title)}}>
-         <img src={img} alt=" "/>
-         <h1 style={{margin: "2rem"}}> {img} </h1>
+        <img className='img' src={img} alt="image not found"/>
+         <h1 style={{margin: "2rem"}}> {pic} </h1>
         <h1 style={{margin: "2rem"}}> {title}</h1>  
         <h3 onClick={()=>{ console.log('alert');}}>{author}</h3>
+        <section className='section' style={{margin: "2rem", display:"flex"}}>
         <button type='button' onClick={clickhandler} style={{margin: "2rem "}}>buy </button>
         <button type='button' onClick={clickhandler} style={{margin: "2rem "}}>add </button>
+        </section>
         </artcile> 
 )
 }
--- src/books.js
+++ src/books.js
@@ -1,29 +1,34 @@
 export const books=[             
  {
 id:1,
-img:'Nord Stage 3 88 keys',
+img:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Steinway_%26_Sons_concert_grand_piano%2C_model_D-274%2C_manufactured_at_Steinway%27s_factory_in_Hamburg%2C_Germany.png/1200px-Steinway_%26_Sons_concert_grand_piano%2C_model_D-274%2C_manufactured_at_Steinway%27s_factory_in_Hamburg%2C_Germany.png",
+pic:'Nord Stage 3 88 keys',
 title:'E.M.L.A Music',
 author:'$5,738.00 Aud'
 
 },
 {
 id:2,
-img:'Line 6 Helix Rack Guitar Processor - 1x',
+img:"https://m.media-amazon.com/images/I/71OXkf-hRxL._SY879_.jpg",
+pic:'Line 6 Helix Rack Guitar Processor - 1x',
 title:'ProMusic',
 author:'$1,831.57 Aud'
 
 },
 {
 id:3,
-img:'Nord Lead A1',
+img:"https://cdn.shopaccino.com/johns-music/products/256fm-1-153899_l.jpg",
+pic:'Nord Lead A1',
 title:'Gear Garage',
 author:'$1700 Aud'
 
 },
 {
 id:1,
-img:'Moog Subsequent 37',
+img:"https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1440,f_auto/MA_00101324_rroxgf.jpg",
+pic:'Moog Subsequent 37',
 title:'Turnlab',
 author:'$5,738 Aud'
+
 }
 ];
