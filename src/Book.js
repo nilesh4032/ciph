a90c1c2e43ff06723f620f6e157e04d4b62b859e
haymaker00 <nileshkumar4032@gmail.com>
2022-07-01 17:48:58 +0530

    1st

--- src/Book.js
+++ src/Book.js
@@ -1,20 +1,12 @@
 import React from 'react'
 
 const Book = ({...props}) => {
-  const {pic,img,title,author}=props;
-    const clickhandler=()=>{
-        alert('done')
-    }
+  const {img,title,author}=props;
+    
 return(
     <artcile className="book" onMouseOver={()=>{ console.log(title)}}>
-        <img className='img' src={img} alt="image not found"/>
-         <h1 style={{margin: "2rem"}}> {pic} </h1>
-        <h1 style={{margin: "2rem"}}> {title}</h1>  
-        <h3 onClick={()=>{ console.log('alert');}}>{author}</h3>
-        <section className='section' style={{margin: "2rem", display:"flex"}}>
-        <button type='button' onClick={clickhandler} style={{margin: "2rem "}}>buy </button>
-        <button type='button' onClick={clickhandler} style={{margin: "2rem "}}>add </button>
-        </section>
+         <img style={{width:"100px",height:"100px"}} src={img} alt='image not found' />
+        <h1 > {title}</h1>  
         </artcile> 
 )
 }
--- src/books.js
+++ src/books.js
@@ -1,34 +1,38 @@
 export const books=[             
  {
-id:1,
-img:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Steinway_%26_Sons_concert_grand_piano%2C_model_D-274%2C_manufactured_at_Steinway%27s_factory_in_Hamburg%2C_Germany.png/1200px-Steinway_%26_Sons_concert_grand_piano%2C_model_D-274%2C_manufactured_at_Steinway%27s_factory_in_Hamburg%2C_Germany.png",
-pic:'Nord Stage 3 88 keys',
-title:'E.M.L.A Music',
-author:'$5,738.00 Aud'
+     id:1,
+    img:'https://m.media-amazon.com/images/M/MV5BMWEwNjhkYzYtNjgzYy00YTY2LThjYWYtYzViMGJkZTI4Y2MyXkEyXkFqcGdeQXVyNTM0OTY1OQ@@._V1_FMjpg_UX1000_.jpg',
+  title:'Uncharted',
+ 
+ },
+ {
+     id:2,
+    img:'https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg',
+  title:'Blackwidow',
 
-},
-{
-id:2,
-img:"https://m.media-amazon.com/images/I/71OXkf-hRxL._SY879_.jpg",
-pic:'Line 6 Helix Rack Guitar Processor - 1x',
-title:'ProMusic',
-author:'$1,831.57 Aud'
+ },
+ {
+     id:2,
+    img:'https://upload.wikimedia.org/wikipedia/en/2/29/Movie_poster_for_%22Scary_Movie%22.jpg',
+  title:'Scary Movie',
+ 
+ },
+ {
+     id:2,
+    img:'https://lumiere-a.akamaihd.net/v1/images/p_aladdin2019_17638_d53b09e6.jpeg',
+  title:'Alladin',
 
-},
-{
-id:3,
-img:"https://cdn.shopaccino.com/johns-music/products/256fm-1-153899_l.jpg",
-pic:'Nord Lead A1',
-title:'Gear Garage',
-author:'$1700 Aud'
+ },
+ {
+     id:2,
+    img:'https://upload.wikimedia.org/wikipedia/en/thumb/1/14/Tenet_movie_poster.jpg/220px-Tenet_movie_poster.jpg',
+  title:'Tenet',
 
-},
-{
-id:1,
-img:"https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1440,f_auto/MA_00101324_rroxgf.jpg",
-pic:'Moog Subsequent 37',
-title:'Turnlab',
-author:'$5,738 Aud'
+ },
+ {
+     id:2,
+    img:'https://static.toiimg.com/photo/91472083.jpeg',
+  title:'Major',
 
-}
+ }
 ];
